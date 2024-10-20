a0831c8bbf8568369e8842c6e46a9d7637e7a7f1
garethbreeze1993 <gareth.breeze1993@gmail.com>
2024-10-20 15:44:18 +0800

    Change title in templates and make sure detailview only gets called when a int is passed to the url

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.QuizListView.as_view(), name="index"),
-    path('<pk>/', views.get_quiz_data, name='q_detail'),
+    path('<int:pk>', views.get_quiz_data, name='q_detail'),
 ]
