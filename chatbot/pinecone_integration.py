f7a6a48c84b45cfb95bff05eb21d64f7f33a99dc
garethbreeze1993 <gareth.breeze1993@gmail.com>
2025-01-07 18:13:14 +0800

    Start chatbot project and make a python script to upload PDF embeddings to pinecone index

---
+++ chatbot/pinecone_integration.py
@@ -0,0 +1,30 @@
+import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pinecone import Pinecone, ServerlessSpec
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_unstructured import UnstructuredLoader
+
+
+
+
+def main():
+
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.getenv("OPENAI_API_KEY"))
+
+    vector_store = PineconeVectorStore(index_name="lyl-pdf", embedding=embeddings,
+                                       pinecone_api_key=os.getenv("PINECONE_API_KEY"))
+
+    path = '/home/gareth/Documents/lyl'
+
+    loader = DirectoryLoader(path=path, glob="*.pdf")
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    all_splits = text_splitter.split_documents(docs)
+    page_content_list = [x.page_content for x in all_splits]
+    _ = vector_store.add_texts(texts=page_content_list)
+
+if __name__ == '__main__':
+    main()
+
