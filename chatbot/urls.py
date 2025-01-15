fd13896aede817ce8c46f5e78af4491234869c10
garethbreeze1993 <gareth.breeze1993@gmail.com>
2025-01-15 13:22:04 +0800

    Can now save a chat get chat details from saved chat and delete a chat

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,7 +5,9 @@ from chatbot import views
 
 urlpatterns = [
     path("", views.ChatListView.as_view(), name="chat_index"),
+    path('<int:pk>', views.get_chat_data, name='chat_detail'),
     path("new_chat", views.chatbot_new_chat, name="new_chat"),
     path("answer_user", views.answer_user_input, name="answer_user_input"),
-    path("save_chat", views.save_chat, name="save_chat")
+    path("save_chat", views.save_chat, name="save_chat"),
+    path('delete/<int:pk>', views.ChatDeleteView.as_view(), name='delete_chat')
 ]
--- chatbot/views.py
+++ chatbot/views.py
@@ -2,13 +2,15 @@ import logging
 import json
 
 from django.contrib import messages
-from django.shortcuts import redirect, render
-from django.views.generic.base import TemplateView
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.views.generic.list import ListView
+from django.views.generic.edit import DeleteView
 from django.http import HttpResponseForbidden
+from django.urls import reverse_lazy
+
 
 from chatbot.helpers import chatbot_response
 from chatbot.models import Chat, Message
@@ -55,7 +57,7 @@ def answer_user_input(request):
 
     chatbot_res_content = chatbot_res.content
 
-    message_dict = {f"user_msg_{chat_number}": user_message, f"llm_msg_{chat_number}": chatbot_res_content}
+    message_dict = {f"user_msg": user_message, f"llm_msg": chatbot_res_content, "chat_number": chat_number}
 
     chat_messages = request.session.get("lyl_messages", [])
     chat_messages.append(message_dict)
@@ -75,8 +77,95 @@ def answer_user_input(request):
 def save_chat(request):
     if request.method != 'POST':
         return HttpResponseForbidden('DONT HIT THIS')
-    logger.info(request.POST['name_title'])
-    logger.info(request.session["lyl_messages"])
-    logger.info(request.session["number_chats"])
+
+    logger.debug(request.POST['name_title'])
+    logger.debug(request.session["lyl_messages"])
+    logger.debug(request.session["number_chats"])
+
+    new_chat = Chat()
+    new_chat.user = request.user
+    new_chat.title = request.POST['name_title']
+
+    try:
+        new_chat.save()
+    except Exception as e:
+        logger.error(e)
+        messages.error(request, f"An error occurred: {str(e)}")
+        return JsonResponse({"error": "Error when saving chat"}, status=500)
+
+    for message in request.session["lyl_messages"]:
+
+        user_chat_number = message["chat_number"] * 2 - 1
+        llm_chat_number = message["chat_number"] * 2
+
+        new_user_message = Message()
+        new_llm_message = Message()
+
+        new_user_message.chat = new_chat
+        new_llm_message.chat = new_chat
+
+        new_user_message.message_text = message['user_msg']
+        new_user_message.order_number = user_chat_number
+        new_user_message.llm_response = False
+
+        new_llm_message.message_text = message['llm_msg']
+        new_llm_message.order_number = llm_chat_number
+        new_llm_message.llm_response = True
+
+        try:
+            new_user_message.save()
+            new_llm_message.save()
+
+        except Exception as e:
+            logger.error(e)
+            messages.error(request, f"An error occurred: {str(e)}")
+            return JsonResponse({"error": "Error when saving Messaged"}, status=500)
+
     messages.success(request, "Data saved successfully!")
     return redirect("index")
+
+@login_required(login_url='login')
+def get_chat_data(request, pk):
+
+    logger.debug("testing output")
+    logger.debug(pk)
+    # Get the quiz by pk or return 404 if not found
+    chat = get_object_or_404(Chat, pk=pk, user=request.user)
+
+    # # Check if the logged-in user is associated with the quiz
+    # if quiz.user != request.user:
+    #     return HttpResponseForbidden("You are not allowed to access this quiz.")
+
+    logger.debug(chat)
+
+    # Get the questions associated with this quiz
+    chat_messages = Message.objects.filter(chat=chat).order_by('order_number')
+
+
+
+    # Send the structured data to the template
+    context = {
+        'chat': chat,
+        'llm_messages': chat_messages
+    }
+
+    return render(request, 'chatbot/chat_detail.html', context)
+
+class ChatDeleteView(LoginRequiredMixin, DeleteView):
+    # specify the model you want to use
+    model = Chat
+    # can specify success url
+    # url to redirect after successfully
+    # deleting object
+    success_url = reverse_lazy("chat_index")
+    template_name = "chatbot/confirm_chat_delete.html"
+    def get_queryset(self):
+        """
+        Limit the queryset to quizzes owned by the logged-in user.
+        """
+        return Chat.objects.filter(user=self.request.user)
+    def handle_no_permission(self):
+        """
+        Handle unauthorized access attempts.
+        """
+        raise Http404("You do not have permission to delete this quiz.")
