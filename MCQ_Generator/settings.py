f68964b772d66da86dc8c8356cb98fdfffee9160
garethbreeze1993 <gareth.breeze1993@gmail.com>
2025-01-29 13:12:06 +0800

    Install gunicorn and change settings to use .env variables instead of hardcoded ready for prod env

--- MCQ_Generator/settings.py
+++ MCQ_Generator/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$ub_t=wcu#jp&f=(je6rc=+8*he^(k(%08t5933tlfrnt9v3^'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if env('DJANGO_ENV') == 'DEVELOPMENT' else False
 
 ALLOWED_HOSTS = []
 
